15b6738298ad207af4b089ce9bc735bf7cc4c8e2
Eugene Dryahlov <egenius333@gmail.com>
2017-08-25 16:16:56 +0400

    ver 1.4 bootstrap 4

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,11 +6,11 @@ ADMIN ENQUEUE FUNCTIONS
 ========================
 */
 function my_load_admin_scripts(){
-    wp_enqueue_style( 'roboto', '//fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic' );
-    wp_enqueue_style( 'my_admin', get_template_directory_uri() . '/css/my.admin.css', array(), CSS_VERSION, 'all' );
-    wp_enqueue_script( 'my-admin-script', get_template_directory_uri() . '/js/my.admin.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_style('roboto','//fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic');
+    wp_enqueue_style('my_admin',get_template_directory_uri().'/css/my.admin.min.css',array(),CSS_VERSION,'all');
+    wp_enqueue_script('my-admin-script',get_template_directory_uri().'/js/my.admin.js',array('jquery'),'1.0.0',true);
 }
-add_action( 'admin_enqueue_scripts', 'my_load_admin_scripts' );
+add_action('admin_enqueue_scripts','my_load_admin_scripts');
 
 /*
 ========================
@@ -18,8 +18,15 @@ FRONT-END ENQUEUE FUNCTIONS
 ========================
 */
 function my_load_scripts(){
-    wp_enqueue_style( 'roboto', '//fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic' );
-    wp_enqueue_style( 'my', get_template_directory_uri() . '/css/my.css', array(), CSS_VERSION, 'all' );
-    wp_enqueue_script( 'my-script', get_template_directory_uri() . '/js/my.admin.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_style('roboto','//fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=cyrillic');
+    //wp_enqueue_style('bootstrap4beta','//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css',false,'4.0.0b');
+    wp_enqueue_style('bootstrap4beta',get_template_directory_uri().'/css/bootstrap4beta.min.css',false,CSS_VERSION);//'4.0.0b'
+    wp_enqueue_style('my',get_template_directory_uri().'/css/my.min.css',false,CSS_VERSION);
+
+    wp_deregister_script( 'jquery' ); //remove default jquery from top
+    wp_enqueue_script('jquery','//code.jquery.com/jquery-3.2.1.slim.min.js',false,'3.2.1',true);
+    wp_enqueue_script('my-script',get_template_directory_uri().'/js/my.admin.js',array('jquery'),'1.0.0',true);
+    wp_enqueue_script('popper','//cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js',array('jquery'),'1.11.0',true);
+    wp_enqueue_script('bootstrap4b','//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js',array('jquery'),'4.0.0b',true);
 }
-add_action( 'wp_enqueue_scripts', 'my_load_scripts' );
+add_action( 'wp_enqueue_scripts', 'my_load_scripts');
--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 
 <h1><?php _e('Index page (/index.php)',DOMAIN);?></h1>
 
+<div class="container">
+	<div class="row">
+        <div class="col"><div class="">1</div></div>
+		<div class="col-auto">
+            <div class="row">
+                <div class="col">sdfsdfs1</div>
+                <div class="col text-nowrap">2sdfsdfsdf sdfsdf sdf</div>
+                <div class="col text-center">3sdf</div>
+                <div class="col">4ffffffffffffffffff</div>
+            </div>
+        </div>
+	</div>
+    <div class="row">
+        <div class="col-xl-6 col-xxxl-12">xxxxx</div>
+    </div>
+</div>
+
 <?php
 if (have_posts()) {
     while (have_posts()) {the_post();
