7a198ec79c4d204a9585057000e52c97ac622944
Eugene Dryahlov <egenius333@gmail.com>
2017-08-31 12:01:28 +0400

    walker & lang fix

--- header.php
+++ header.php
@@ -14,7 +14,10 @@
 
     $lang_path = parse_url($_SERVER['REQUEST_URI'])['path'];
     if (strstr($_SERVER['REQUEST_URI'],'_rus.html?lang=rus')) {$lang_path = str_replace('_rus.html','.html',$lang_path);}
-    else {$lang_path = str_replace('.html','_rus.html',$lang_path);}
+    else {
+        $lang_path = str_replace('_rus','',$lang_path); //cleanup
+        $lang_path = str_replace('.html','_rus.html',$lang_path);
+    }
 
     LANG == 'eng' ? $lang_choice = '<a href="'.$lang_path.'?lang=rus">RUS</a>' : $lang_choice = '<a href="'.$lang_path.'?lang=eng">ENG</a>';
     echo $lang_choice;
@@ -31,8 +34,6 @@
                     'echo'            => false, //leave it for processing via regexp
                     'container'       => 'nav',
                     'container_class' => 'row',
-//                    'before'          => '<div class="col text-nowrap">',
-//                    'after'           => '</div>',
                     'items_wrap'      => '%3$s',
                     'depth'           => 0,
                     'walker'          => new Walker_Nav_Menu1
