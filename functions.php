6b4705e3f820de461cf8fe42df3ed9d4e463f4fc
Eugene Dryahlov <egenius333@gmail.com>
2017-08-31 11:44:46 +0400

    add menu walker

--- functions.php
+++ functions.php
@@ -8,5 +8,6 @@ require get_template_directory().'/inc/enqueue.php'; //enqueueing the scripts an
 require get_template_directory().'/inc/translation.php'; //translation module
 require get_template_directory().'/inc/html-on-pages.php'; //adds .html to pages
 require get_template_directory().'/inc/theme-support.php';
+require get_template_directory().'/inc/nav-menu-walker.php';
 
 
